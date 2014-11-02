88c12e089b4f420e6e6cfbc275f11efdda7bd440
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2014-11-02 19:10:39 +0000

    added two systems for fragmentation: bimotor and rabbits

---
+++ src/test/scala/graph-rewriting/Bimotor.scala
@@ -0,0 +1,51 @@
+package graph_rewriting
+
+import org.scalatest.{FlatSpec, Matchers}
+import implicits._
+
+class BimotorSpec extends FlatSpec with Matchers {
+
+  "A bimotor" should "walk on DNA" in {
+    val bc0 = "b" ~~> "c1"
+    val bc1 = "b" ~~> "c1"
+    val bc2 = "b" ~~> "c2"
+    val bc3 = "b" ~~> "c2"
+    val c1c2 = "c1" ~~> "c2"
+    val g1 = Graph("b" -> "bimotor", "c1" -> "chain", "c2" -> "chain")(
+      bc0, bc1, c1c2)
+    val g2 = Graph("b" -> "bimotor", "c1" -> "chain", "c2" -> "chain")(
+      bc1, bc2, c1c2)
+    val g3 = Graph("b" -> "bimotor", "c1" -> "chain", "c2" -> "chain")(
+      bc2, bc3, c1c2)
+    val g4 = Graph("b" -> "bimotor", "c1" -> "chain")(bc0, bc1)
+    val fe = Rule(g1, g2, Map("b" -> "b", "c1" -> "c1", "c2" -> "c2"),
+      Map(c1c2 -> c1c2, bc1 -> bc1), 1)
+    val fc = Rule(g2, g3, Map("b" -> "b", "c1" -> "c1", "c2" -> "c2"),
+      Map(c1c2 -> c1c2, bc2 -> bc2), 100)
+    val bc = fe.reversed; bc.rate = 10
+    val be = fc.reversed; be.rate = 1000
+
+    object mfa extends MFA[String,String] {
+
+      // Transformations
+      def invariant(g: G): Option[P] =
+        if (Graph.iso(g, g1) || Graph.iso(g, g3)) Some(1.0 * g4)
+        else None
+
+      def reachable(g: G): Option[P] = {
+        val bs = g.nodes collect { case n if g(n).label == Some("bimotor") => n }
+        val cs = g.nodes collect { case n if g(n).label == Some("chain") => n }
+        if ((bs.size == 1) && (bs forall (g(_).outDegree == 2)) &&
+            (cs.size <= 2) && (cs.toSeq.combinations(2) forall {
+              case Seq(u, v) => ((g(u) edgesWith v).size == 1) }))
+          None
+        else Some(Pn.empty)
+      }
+    }
+
+    val eqs = mfa.mfa(List(fe, fc, bc, be), List(g1, g2, g3),
+      mfa.invariant _, mfa.reachable _)
+    eqs.printEqs
+  }
+}
+
---
+++ src/test/scala/graph-rewriting/Rabbits.scala
@@ -0,0 +1,28 @@
+package graph_rewriting
+
+import org.scalatest.{FlatSpec, Matchers}
+import implicits._
+
+class RabbitsSpec extends FlatSpec with Matchers {
+
+  object mfa extends MFA[String,String]
+
+  "Rabbits" should "reproduce" in {
+    val k1 = 1.0
+    val (e1, e2) = ("father" ~~> "daughter", "mother" ~~> "daughter")
+    val parents = Graph("father" -> "R", "mother" -> "R")()
+    val one = Graph("father" -> "R", "mother" -> "R", // one child
+      "daughter" -> "R")(e1, e2)
+    val sex = Rule(parents, one, Map("father" -> "father",
+      "mother" -> "mother"), Map(), k1)
+    val two = Graph("father" -> "R", "mother" -> "R", "daughter" -> "R",
+      "son" -> "R")(e1, e2, "father" ~~> "son", "mother" ~~> "son")
+    val family = Rule(one, two, Map("father" -> "father",
+      "mother" -> "mother", "daughter" -> "daughter"),
+      Map(e1 -> e1, e2 -> e2), 10)
+    val eqs = mfa.mfa(List(sex, family), List(two),
+      mfa.splitConnectedComponents)
+    eqs.printEqs
+  }
+}
+
